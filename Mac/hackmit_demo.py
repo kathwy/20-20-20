df63f053e394321af30a1af0ab7ae802519dfacb
Katherine Young <kayoung@mit.edu>
2016-09-18 11:01:32 -0400

    Update hackmit_demo.py

--- Mac/hackmit_demo.py
+++ Mac/hackmit_demo.py
@@ -3,8 +3,8 @@
 import os
 import time
 import tkinter
-from tkinter import messagebox
 from tkinter import *
+from tkinter import messagebox
 
 def notify(title, subtitle, message):
     '''
@@ -17,13 +17,24 @@ def notify(title, subtitle, message):
 
 root = tkinter.Tk()
 root.withdraw()
+widget_list = []
 
 def sleep():
     '''
     Dims screen
     '''
-    os.system("pmset displaysleepnow -t 10; sleep 10; caffeinate -u -t 1") # change for dim time
-    root.withdraw()      
+    root.withdraw()
+    for image in widget_list:
+        image.pack_forget()
+    os.system("pmset displaysleepnow -t 10; sleep 10; caffeinate -u -t 1") # change the numbers for dim time in seconds
+
+def showimage(i):
+        image_dict = {1:"",2:"" ,3:"" ,4: "",5: "5.gif",6:"" ,7:"" ,8:"" ,9:"" ,10:"" ,11:"", 12: "12.gif"}
+        pusheen = PhotoImage(file=image_dict[i])
+        pusheen_context = Label(image=pusheen)
+        pusheen_context.image = pusheen
+        pusheen_context.pack()
+        widget_list.append(pusheen_context)
 
 def main():
     '''
@@ -31,30 +42,31 @@ def main():
     If they don't comply, send reminders/messages/fun facts every 10 minutes until they do
     '''
 
-    latent_time = 20 # 1200 sec = 20 min in between rests
-    time_between_reminders = 10 # 600 sec = 10 min in between reminders
+    latent_time = 10 # 1200 sec = 20 min in between rests
+    time_between_reminders = 5 # 600 sec = 10 min in between reminders
     prompt_message = "Do you want to take a rest?"
-    final_message = "Wow, we've run out of sass to sass you with and that says something: You REALLY need to rest your eyes..."
     messages =  {1: "Pusheen would want you to take a break too.",
                  2: "It's been 30 minutes since your last break. Just look away. It's not that hard.",
-                 3: "Remember, how we told you that spending hours on a computer can strain the eye muscles to cause headaches? I foresee a headache \
-                coming your way...",
+                 3: "Spending hours on a computer can strain the eye muscles to cause headaches. " + 
+                    "I spy a headache coming your way...",
                  4: "If you go blind, from not resting, you won't be able to enjoy our sassy messages. Wouldn't that be sad?",
                  5: "Bro.",
-                 6: "Maybe you need some factual motivation: Your blink rate slows down when you look at things that are closer to your face \
-                    (like your laptop), exacerbating dry eyes and itchiness. Do you suffer from dry eyes? Now, you know why. Take a break.",
+                 6: "Maybe you need some factual motivation: Your blink rate slows down when you look at things that are closer to your face (like your laptop)," + 
+                    " exacerbating dry eyes and itchiness. Do you suffer from dry eyes? Now, you know why. Take a break.",
                  7: "You know.. it's been...80 minutes since your last break? What are you doing that's so time-sensitive? Or are you just lazy?",
-                 8: "Here's a fact to motivate you, you poor sufferer of eye strain... \
-                    'A large majority, nearly 70%, of Millennials report symptoms of digital eye strain.' Don't be in that 70%.",
+                 8: "Here's a fact to motivate you, you poor sufferer of eye strain..." + 
+                    "'A large majority, nearly 70%, of Millennials report symptoms of digital eye strain.' Don't be in that 70%.",
                  9: "Don't go blind. I don't want you to go blind... because that'd mean my efforts were for naught.",
-                 10: "If you don't rest your eyes soon, you may start to notice: blurred vision, double vision, dry red eyes, eye irritation, \
-                    headaches, neck or back pain...and maybe even death...",
+                 10: "If you don't rest your eyes soon, you may start to notice: blurred vision, double vision, dry red eyes, eye irritation," + 
+                     "headaches, neck or back pain...and maybe even death...",
                  11: "Dude.",
+                 12: "Wow, we've run out of sass to sass you with and that says something: You REALLY need to rest your eyes...",
                  'title': 'Eye Break',
                  'subtitle': 'With 20/20/20',
                  'message_20': "It's been 20 min! Click on the Python Eye-Con to take a 20 sec break!",
                  'message_10': "It's been another 10 min! Click on the Python Eye-Con to take a 20 sec break!"
                  }    
+    number_messages = 12
 
     sass_factor = 0 # number of times user hit snooze 
     complied = False
@@ -64,7 +76,6 @@ def main():
             notify(messages['title'],
                    messages['subtitle'],
                    messages['message_20'])
-           # root.protocol("WM_DELETE_WINDOW")
         if tkinter.messagebox.askyesno("EYE BREAK", prompt_message):
             complied = True
             sleep()
@@ -72,6 +83,9 @@ def main():
             sass_factor = 0
         else:
             complied = False
+            showimage(sass_factor + 1)
+            root.update()
+            root.deiconify()
             sass_factor += 1
             time.sleep(time_between_reminders)
             if sass_factor != 0:
@@ -84,6 +98,7 @@ def main():
                     notify(messages['title'],
                            messages['subtitle'],
                            messages['message_10'])
-                    tkinter.messagebox.showinfo("FUN FACT", final_message)
+                    tkinter.messagebox.showinfo("FUN FACT", str(messages[number_nessages]))
+
 
 main()
