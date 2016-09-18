510458000400d24c102f95001be8f1bcbef15a97
Katherine Young <kayoung@mit.edu>
2016-09-18 16:40:02 -0400

    Update hackmit_demo.py
    Added gifs

--- Mac/hackmit_demo.py
+++ Mac/hackmit_demo.py
@@ -26,34 +26,33 @@ def sleep():
     root.withdraw()
     for image in widget_list:
         image.pack_forget()
-    os.system("pmset displaysleepnow -t 1200; sleep 1200; caffeinate -u -t 1") # change the numbers for dim time in seconds
+    os.system("pmset displaysleepnow -t 10; sleep 10; caffeinate -u -t 1") # change the numbers for dim time in seconds
 
-def showimage(i):
-        # you can add more gifs to go along with your messages
-        image_dict = {1:"", 2:"", 3:"", 4:"", 5:"5.gif", 6:"", 7:"", 8:"", 9:"", 10:"", 11:"", 12:"12.gif"}
-        pusheen = PhotoImage(file=image_dict[i])
-        pusheen_context = Label(image=pusheen)
-        pusheen_context.image = pusheen
-        pusheen_context.pack()
-        widget_list.append(pusheen_context)
+def show_image(i):
+    # you can change the gifs to go along with your messages
+    image_dict = {1:"", 2:"", 3:"3.gif", 4:"4.gif", 5:"5.gif", 6:"", 7:"", 8:"8.gif", 9:"9.gif", 10:"", 11:"", 12:"12.gif"}
+    pusheen = PhotoImage(file=image_dict[i])
+    pusheen_context = Label(image=pusheen) 
+    pusheen_context.image = pusheen
+    pusheen_context.pack()
+    widget_list.append(pusheen_context)
 
 def main():
     '''
     Prompts user to take a rest. If they comply, dim screen for 20 seconds
-    If they don't comply, send reminders/messages/fun facts every 10 minutes until they do
+    If they don't comply, send reminders/gifs/fun facts every 10 minutes until they do
     '''
-
-    latent_time = 1200 # 1200 sec = 20 min in between rests
-    time_between_reminders = 600 # 600 sec = 10 min in between reminders
+    latent_time = 10 # 1200 sec = 20 min in between rests
+    time_between_reminders = 2 # 600 sec = 10 min in between reminders
     prompt_message = "Do you want to take a rest?"
-    messages =  {1: "Pusheen would want you to take a break too.",
+    messages =  {1: "The prevalence of nearsightedness among Americans has increased by 66% over the past 30 years. Give your eyes a rest.",
                  2: "It's been 30 minutes since your last break. Just look away. It's not that hard.",
                  3: "Spending hours on a computer can strain the eye muscles to cause headaches. " + 
                     "I spy a headache coming your way...",
                  4: "If you go blind, from not resting, you won't be able to enjoy our sassy messages. Wouldn't that be sad?",
                  5: "Bro.",
                  6: "Maybe you need some factual motivation: Your blink rate slows down when you look at things that are closer to your face (like your laptop), " + 
-                    " exacerbating dry eyes and itchiness. Do you suffer from dry eyes? Now, you know why. Take a break.",
+                    "exacerbating dry eyes and itchiness. Do you suffer from dry eyes? Now, you know why. Take a break.",
                  7: "You know.. it's been...80 minutes since your last break? What are you doing that's so time-sensitive? Or are you just lazy?",
                  8: "Here's a fact to motivate you, you poor sufferer of eye strain... " + 
                     "'A large majority, nearly 70%, of Millennials report symptoms of digital eye strain.' Don't be in that 70%.",
@@ -77,6 +76,10 @@ def main():
             notify(messages['title'],
                    messages['subtitle'],
                    messages['message_20'])
+        if sass_factor < number_messages:
+            show_image(sass_factor + 1)
+        root.update()
+        root.deiconify()
         if tkinter.messagebox.askyesno("EYE BREAK", prompt_message):
             complied = True
             sleep()
@@ -84,13 +87,10 @@ def main():
             sass_factor = 0
         else:
             complied = False
-            showimage(sass_factor + 1)
-            root.update()
-            root.deiconify()
             sass_factor += 1
             time.sleep(time_between_reminders)
             if sass_factor != 0:
-                if sass_factor <= len(messages):
+                if sass_factor <= number_messages:
                     notify(messages['title'],
                            messages['subtitle'],
                            messages['message_10'])
@@ -99,7 +99,7 @@ def main():
                     notify(messages['title'],
                            messages['subtitle'],
                            messages['message_10'])
-                    tkinter.messagebox.showinfo("FUN FACT", str(messages[number_nessages]))
+                    tkinter.messagebox.showinfo("FUN FACT", str(messages[number_messages]))
 
 
 main()
